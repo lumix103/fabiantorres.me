5fec750596160f95bfb2e395a1478851c74c2f88
Fabs <15038234+lumix103@users.noreply.github.com>
2025-03-24 14:45:39 -0700

    Added SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,18 @@ const geistSans = Geist({ subsets: ["latin"] });
 const geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Fabs",
-  description: "Fabs's personal website",
+  title: "Fabs' Portfolio",
+  description: "Fabs' personal website",
+  openGraph: {
+    images: [
+      {
+        url: "https://www.fabiantorres.me/images/logo.png",
+        width: 500,
+        height: 500,
+        alt: "Fabs' Portfolio Thumbnail",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
